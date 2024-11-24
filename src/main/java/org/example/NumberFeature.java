6a46bdc30cbbc9274348c2163d38f64c6b345044
Ww318 <156657956+Ww318@users.noreply.github.com>
2024-11-24 21:06:30 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class NumberFeature {
     public static void main(String[] args) {
-        int inputNumber; // 输入的数字
-        int result = 0; // 输出结果
         Scanner in = new Scanner(System.in);
-        inputNumber = in.nextInt();
-        // 你的代码逻辑
+        int inputNumber = in.nextInt();
+        int result = 0;
+
+        // 计算奇偶性特征对应的二进制值的各位之和
+        while (inputNumber > 0) {
+            int digit = inputNumber % 10;
+            if (digit % 2 == 0) {
+                result += 0;
+            } else {
+                result += 1;
+            }
+            inputNumber /= 10;
+        }
 
         System.out.println(result);
     }
