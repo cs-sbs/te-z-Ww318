9741961d0987cfc4ab1be2c1f60ee99b0c3e83b3
Ww318 <3183886708@qq.com>
2024-11-25 15:35:32 +0800

    Update NumberFeature.java

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -1,25 +1,32 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OddEven {
     public static void main(String[] args) {
-        int number;
         int countOdd = 0;
         int countEven = 0;
-        Scanner in = new Scanner(System.in); // 数字用空格或换行隔开，每次读入一个，直到输入‘-1’
-        do{
-            number = in.nextInt();
-            if(number != -1)
-            {
-                if (number % 2 == 0) {
-                    countEven += 1;
-                } else {
-                    countOdd += 1;
+        Scanner in = new Scanner(System.in);
+        // 使用while循环
+        while (true) {
+            try {
+                System.out.print("请输入一个整数（输入 - 1结束）：");
+                int number = in.nextInt();
+                if (number == -1) {
+                    break;
                 }
+                if (number % 2 == 0) {
+                    countEven++;
+                } else {
+                    countOdd++;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("输入有误，请输入整数！");
+                in.nextLine();
             }
-        }while(number != -1); // 理解do while和while循环的区别
-        System.out.print(countOdd+" ");
-        System.out.println(countEven);
+        }
+        System.out.println("奇数个数：" + countOdd);
+        System.out.println("偶数个数：" + countEven);
     }
 }
